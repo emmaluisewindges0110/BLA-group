7b1a672e8be307ff7b32ac04e46967fd4560f88a
Paul <p.tirler@gmail.com>
2023-10-05 18:29:41 +0200

    Test

--- src/matrix.h
+++ src/matrix.h
@@ -1,75 +1,63 @@
-#ifndef FILE_VECTOR_H
-#define FILE_VECTOR_H
-
+#ifndef FILE_MATRIX_H
+#define FILE_MATRIX_H
+#include <vector>
 #include <iostream>
 
 namespace ASC_bla
 {
-  
-  template <typename T>
-  class Vector
+enum ORDERING { ColMajor, RowMajor };
+
+template <typename T, ORDERING ORD>
+class Matrix
   {
-    size_t size_;
-    T * data_;
-    
-  public:
-    Vector (size_t size) 
-      : size_(size), data_(new T[size]) { ; }
-    
-    Vector (const Vector & v)
-      : Vector(v.Size())
-    {
-      *this = v;
+   private:
+    std::vector<T> data;
+    int rows, cols;
+
+public:
+    // Constructor
+    Matrix(int numRows, int numCols) : rows(numRows), cols(numCols) {
+        data.resize(rows * cols);
     }
 
-    Vector (Vector && v)
-      : size_{0}, data_(nullptr)
-    {
-      std::swap(size_, v.size_);
-      std::swap(data_, v.data_);
+    // Destructor
+    ~Matrix() {}
+
+    // Method for matrix multiplication
+    Matrix<T, ORD> multiply(const Matrix<T, ORD>& other) const {
+        if (cols != other.rows) {
+            std::cerr << "Matrix dimensions are not compatible for multiplication." << std::endl;
+            exit(1);
+        }
+
+        int resultRows = rows;
+        int resultCols = other.cols;
+        Matrix<T, ORD> result(resultRows, resultCols);
+
+        if (ORD == ColMajor) {
+            for (int i = 0; i < resultRows; ++i) {
+                for (int j = 0; j < resultCols; ++j) {
+                    T sum = 0;
+                    for (int k = 0; k < cols; ++k) {
+                        sum += data[i * cols + k] * other.data[k * other.cols + j];
+                    }
+                    result.data[i * resultCols + j] = sum;
+                }
+            }
+        } else if (ORD == RowMajor) {
+            for (int i = 0; i < resultRows; ++i) {
+                for (int j = 0; j < resultCols; ++j) {
+                    T sum = 0;
+                    for (int k = 0; k < cols; ++k) {
+                        sum += data[i * cols + k] * other.data[j * other.cols + k];
+                    }
+                    result.data[i * resultCols + j] = sum;
+                }
+            }
+        }
+
+        return result;
     }
-
-    ~Vector () { delete [] data_; }
-    
-    Vector & operator=(const Vector & v2)
-    {
-      for (size_t i = 0; i < size_; i++)
-        data_[i] = v2(i);
-      return *this;
-    }
-
-    Vector & operator= (Vector && v2)
-    {
-      for (size_t i = 0; i < size_; i++)
-        data_[i] = v2(i);
-      return *this;
-    }
-    
-    size_t Size() const { return size_; }
-    T & operator()(size_t i) { return data_[i]; }
-    const T & operator()(size_t i) const { return data_[i]; }
-  };
-
-
-  template <typename T>
-  Vector<T> operator+ (const Vector<T> & a, const Vector<T> & b)
-  {
-    Vector<T> sum(a.Size());
-    for (size_t i = 0; i < a.Size(); i++)
-      sum(i) = a(i)+b(i);
-    return sum;
-  }
-  
-  template <typename T>
-  std::ostream & operator<< (std::ostream & ost, const Vector<T> & v)
-  {
-    if (v.Size() > 0)
-      ost << v(0);
-    for (size_t i = 1; i < v.Size(); i++)
-      ost << ", " << v(i);
-    return ost;
-  }
-  
+};
 }
-
 #endif
