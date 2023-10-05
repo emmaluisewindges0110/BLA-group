1e219e288403b57c902bd44bd564add6101049d8
Paul <p.tirler@gmail.com>
2023-10-06 00:31:06 +0200

    Sollte in etwa funktionieren

--- src/matrix.h
+++ src/matrix.h
@@ -1,63 +1,281 @@
 #ifndef FILE_MATRIX_H
 #define FILE_MATRIX_H
-#include <vector>
+
 #include <iostream>
+#include <vector.h>
 
 namespace ASC_bla
 {
 enum ORDERING { ColMajor, RowMajor };
 
-template <typename T, ORDERING ORD>
+template <typename R, ORDERING ORD>
 class Matrix
-  {
-   private:
-    std::vector<T> data;
-    int rows, cols;
+{
+    size_t rows_;
+    size_t columns_;
+    R **data_;
 
 public:
-    // Constructor
-    Matrix(int numRows, int numCols) : rows(numRows), cols(numCols) {
-        data.resize(rows * cols);
+    // Constructor to create a matrix with specified rows and columns
+    Matrix(size_t rows, size_t columns)
+        : rows_(rows), columns_(columns)
+    {
+        data_ = new R *[rows_];
+        for (size_t i = 0; i < rows_; ++i)
+        {
+            data_[i] = new R[columns_];
+        }
+    }
+
+    // Copy constructor
+    Matrix(const Matrix &other)
+        : Matrix(other.rows_, other.columns_)
+    {
+        for (size_t i = 0; i < rows_; ++i)
+        {
+            for (size_t j = 0; j < columns_; ++j)
+            {
+                if (ORD == ColMajor)
+                {
+                    data_[i][j] = other.data_[i][j];
+                }
+                else
+                {
+                    data_[i][j] = other.data_[j][i];
+                }
+            }
+        }
+    }
+
+    // Move constructor
+    Matrix(Matrix &&other)
+        : rows_{0}, columns_{0}, data_(nullptr)
+    {
+        std::swap(rows_, other.rows_);
+        std::swap(columns_, other.columns_);
+        std::swap(data_, other.data_);
     }
 
     // Destructor
-    ~Matrix() {}
+    ~Matrix()
+    {
+        for (size_t i = 0; i < rows_; ++i)
+        {
+            delete[] data_[i];
+        }
+        delete[] data_;
+    }
+    // Overload () operator to access elements of the matrix
+R& operator() (size_t row, size_t col)
+{
+    if (ORD == ColMajor)
+    {
+        return data_[row][col];
+    }
+    else // ORD == RowMajor
+    {
+        return data_[col][row];
+    }
+}
 
-    // Method for matrix multiplication
-    Matrix<T, ORD> multiply(const Matrix<T, ORD>& other) const {
-        if (cols != other.rows) {
-            std::cerr << "Matrix dimensions are not compatible for multiplication." << std::endl;
-            exit(1);
+// Overload () operator for const access to elements
+const R& operator() (size_t row, size_t col) const
+{
+    if (ORD == ColMajor)
+    {
+        return data_[row][col];
+    }
+    else // ORD == RowMajor
+    {
+        return data_[col][row];
+    }
+}
+
+// Overload = operator for assignment
+Matrix<R, ORD>& operator= (const Matrix<R, ORD>& other)
+{
+    if (this != &other)
+    {
+        // Deallocate existing data if any
+        for (size_t i = 0; i < rows_; ++i)
+        {
+            delete[] data_[i];
+        }
+        delete[] data_;
+
+        // Copy dimensions
+        rows_ = other.rows_;
+        columns_ = other.columns_;
+
+        // Allocate new data
+        data_ = new R *[rows_];
+        for (size_t i = 0; i < rows_; ++i)
+        {
+            data_[i] = new R[columns_];
         }
 
-        int resultRows = rows;
-        int resultCols = other.cols;
-        Matrix<T, ORD> result(resultRows, resultCols);
+        // Copy data
+        for (size_t i = 0; i < rows_; ++i)
+        {
+            for (size_t j = 0; j < columns_; ++j)
+            {
+                (*this)(i, j) = other(i, j);
+            }
+        }
+    }
+    return *this;
+}
+// Member function to get the number of rows
+size_t NumRows() const {
+    return rows_;
+}
 
-        if (ORD == ColMajor) {
-            for (int i = 0; i < resultRows; ++i) {
-                for (int j = 0; j < resultCols; ++j) {
-                    T sum = 0;
-                    for (int k = 0; k < cols; ++k) {
-                        sum += data[i * cols + k] * other.data[k * other.cols + j];
-                    }
-                    result.data[i * resultCols + j] = sum;
+// Member function to get the number of columns
+size_t NumColumns() const {
+    return columns_;
+}
+};
+
+
+// Matrix-Matrix Multiplication
+template <typename T, ORDERING ORD>
+Matrix<T, ORD> MatrixMatrixMultiply(const Matrix<T, ORD> &mat1, const Matrix<T, ORD> &mat2)
+{
+    if (mat1.NumColumns() != mat2.NumRows())
+    {
+        throw std::invalid_argument("Matrix dimensions are not compatible for multiplication.");
+    }
+
+    size_t numRows = mat1.NumRows();
+    size_t numCols = mat2.NumColumns();
+    size_t commonDim = mat1.NumColumns();
+
+    Matrix<T, ORD> result(numRows, numCols);
+
+    if (ORD == ColMajor)
+    {
+        for (size_t i = 0; i < numRows; ++i)
+        {
+            for (size_t j = 0; j < numCols; ++j)
+            {
+                T sum = 0;
+                for (size_t k = 0; k < commonDim; ++k)
+                {
+                    sum += mat1(i, k) * mat2(k, j);
                 }
+                result(i, j) = sum;
             }
-        } else if (ORD == RowMajor) {
-            for (int i = 0; i < resultRows; ++i) {
-                for (int j = 0; j < resultCols; ++j) {
-                    T sum = 0;
-                    for (int k = 0; k < cols; ++k) {
-                        sum += data[i * cols + k] * other.data[j * other.cols + k];
-                    }
-                    result.data[i * resultCols + j] = sum;
+        }
+    }
+    else // ORD == RowMajor
+    {
+        for (size_t i = 0; i < numRows; ++i)
+        {
+            for (size_t j = 0; j < numCols; ++j)
+            {
+                T sum = 0;
+                for (size_t k = 0; k < commonDim; ++k)
+                {
+                    sum += mat1(i, k) * mat2(k, j);
                 }
+                result(i, j) = sum;
             }
+        }
+    }
+
+    return result;
+}
+
+// Vector-Matrix Multiplication
+template <typename R, ORDERING ORD>
+Matrix<R, ORD> VectorMatrixMultiply(const Vector<R> &vec, const Matrix<R, ORD> &mat)
+{
+    if (ORD == ColMajor)
+    {
+        if (vec.size() != mat.NumRows())
+        {
+            throw std::invalid_argument("Vector size must match matrix number of rows for multiplication.");
+        }
+
+        size_t numCols = mat.NumColumns();
+        Matrix<R, ORD> result(1, numCols);
+
+        for (size_t j = 0; j < numCols; ++j)
+        {
+            R sum = 0;
+            for (size_t i = 0; i < vec.size(); ++i)
+            {
+                sum += vec[i] * mat(i, j);
+            }
+            result(0, j) = sum;
         }
 
         return result;
     }
-};
+    else // ORD == RowMajor
+    {
+        if (vec.size() != mat.NumColumns())
+        {
+            throw std::invalid_argument("Vector size must match matrix number of columns for multiplication.");
+        }
+
+        size_t numRows = mat.NumRows();
+        Matrix<R, ORD> result(numRows, 1);
+
+        for (size_t i = 0; i < numRows; ++i)
+        {
+            R sum = 0;
+            for (size_t j = 0; j < vec.size(); ++j)
+            {
+                sum += mat(i, j) * vec[j];
+            }
+            result(i, 0) = sum;
+        }
+
+        return result;
+    }
 }
+
+// Transposition of Matrix
+template <typename R, ORDERING ORD>
+Matrix<R, ORD> Transpose(const Matrix<R, ORD> &mat)
+{
+    size_t numRows = mat.NumRows();
+    size_t numCols = mat.NumColumns();
+
+    Matrix<R, ORD> result(numCols, numRows);
+
+    for (size_t i = 0; i < numRows; ++i)
+    {
+        for (size_t j = 0; j < numCols; ++j)
+        {
+            if (ORD == ColMajor)
+            {
+                result(j, i) = mat(i, j);
+            }
+            else // ORD == RowMajor
+            {
+                result(j, i) = mat(i, j);
+            }
+        }
+    }
+
+    return result;
+}
+
+// Overload * operator for Vector-Matrix Multiplication
+template <typename R, ORDERING ORD>
+Matrix<R, ORD> operator* (const Vector<R> &vec, const Matrix<R, ORD> &mat)
+{
+    return VectorMatrixMultiply(vec, mat);
+}
+
+// Overload * operator for Matrix-Matrix Multiplication
+template <typename R, ORDERING ORD>
+Matrix<R, ORD> operator* (const Matrix<R, ORD> &mat1, const Matrix<R, ORD> &mat2)
+{
+    return MatrixMatrixMultiply(mat1, mat2);
+}
+
+};
 #endif
--- tests/test_vector.cc
+++ tests/test_vector.cc
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <matrix.h>
 #include <vector.h>
 
 namespace bla = ASC_bla;
@@ -7,6 +7,8 @@ namespace bla = ASC_bla;
 
 int main()
 {
+
+  //creating Vektor examples
   size_t n = 5;
   bla::Vector<double> x(n), y(n);
 
@@ -15,10 +17,27 @@ int main()
       x(i) = i;
       y(i) = 10;
     }
-
+//demonstrating Vektor Operations
   bla::Vector<double> z = x+y;
   
   std::cout << "x+y = " << z << std::endl;
+  
+//creating Matrix examples
+  bla::Matrix<int, bla::ColMajor> A(5, 5), B(5, 5);
+for (size_t i = 0; i < 5; ++i) {
+        for (size_t j = 0; j < 5; ++j) {
+            A(i, j) = i + j;
+            B(i, j) = 1;
+        }
+    }
+    // Perform matrix multiplication
+    bla::Matrix<int, bla::ColMajor> result = A * B;
+    // Output the result
+    std::cout << "Result of matrix multiplication:\n";
+    for (size_t i = 0; i < 5; ++i) {
+        for (size_t j = 0; j < 5; ++j) {
+            std::cout << result(i, j) << " ";
+        }
+        std::cout << "\n";
+    }
 }
-
-
