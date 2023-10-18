e41043a01883be41be532bdf94bce568aafa9e7f
VoxelPi <verblockt@gmail.com>
2023-10-18 21:29:01 +0200

    Fix build

--- py_tests/test_vector.py
+++ py_tests/test_vector.py
@@ -1,6 +1,6 @@
 # search for libraray like bla.cpython-312-darwin.so in the build directory:
 # import sys
-# sys.path.append('/Users/joachim/texjs/lva/ws2324/ScientificComputing/ASC-bla/build')
+# sys.path.append('/home/peter/uni/scicomp/bla-group/build')
 # from bla import Vector
 
 # import from the installed ASCsoft package:
